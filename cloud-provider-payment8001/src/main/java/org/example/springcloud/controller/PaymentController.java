dfa15cbf19b1a2ef89b9a6529515b2c01bdf4722
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-02 13:10:11 +0800

    消费者订单模块构建

--- cloud-provider-payment8001/src/main/java/org/example/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/org/example/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import org.example.springcloud.entities.CommonResult;
 import org.example.springcloud.entities.Payment;
 import org.example.springcloud.service.impl.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -18,7 +15,7 @@ public class PaymentController {
     private PaymentServiceImpl paymentServiceImpl;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentServiceImpl.create(payment);
         log.info("*****插入结果: " + result);
         if(result > 0) {
