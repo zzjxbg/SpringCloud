0ae88bf501b37b7a92615d51a4d4415df5387d11
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-06 14:07:13 +0800

    actuator微服务信息完善

--- cloud-provider-payment8001/src/main/java/org/example/springcloud/controller/PaymentController8001.java
+++ cloud-provider-payment8001/src/main/java/org/example/springcloud/controller/PaymentController8001.java
@@ -3,7 +3,7 @@ package org.example.springcloud.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.example.springcloud.entities.CommonResult;
 import org.example.springcloud.entities.Payment;
-import org.example.springcloud.service.impl.PaymentServiceImpl;
+import org.example.springcloud.service.impl.PaymentServiceImpl8002;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
@@ -15,10 +15,10 @@ import java.util.List;
 
 @RestController
 @Slf4j
-public class PaymentController {
+public class PaymentController8001 {
 
     @Autowired
-    private PaymentServiceImpl paymentServiceImpl;
+    private PaymentServiceImpl8002 paymentServiceImpl8002;
 
     @Value("${server.port}")
     private String serverPort;
@@ -28,7 +28,7 @@ public class PaymentController {
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
-        int result = paymentServiceImpl.create(payment);
+        int result = paymentServiceImpl8002.create(payment);
         log.info("*****插入结果: " + result);
         if(result > 0) {
             return new CommonResult(200,"插入数据库成功,serverPort: " + serverPort,result);
@@ -39,7 +39,7 @@ public class PaymentController {
 
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
-        Payment payment = paymentServiceImpl.getPaymentById(id);
+        Payment payment = paymentServiceImpl8002.getPaymentById(id);
         log.info("*****插入结果: " + payment);
         if(payment != null) {
             return new CommonResult(200,"查询成功,serverPort: " + serverPort,payment);
