588dedb9fdd8ba62fbdc92e3fa2a708600bd6346
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-08 16:40:41 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/org/example/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,22 @@
+package org.example.springcloud.controller;
+
+import org.example.springcloud.entities.CommonResult;
+import org.example.springcloud.entities.Payment;
+import org.example.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController  //呵呵
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value="/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/org/example/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,21 @@
+package org.example.springcloud.service;
+
+import feign.Param;
+import org.example.springcloud.entities.CommonResult;
+import org.example.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Feign：接口+注解
+ * 微服务调用接口+@FeignClient
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")  //指定调用哪个微服务
+public interface PaymentFeignService {
+
+    @GetMapping(value="/payment/get/{id}")   //哪个地址
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
