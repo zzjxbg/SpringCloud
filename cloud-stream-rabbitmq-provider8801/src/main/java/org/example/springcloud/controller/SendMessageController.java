6f33ead858bd15c1224967e3095a44cf72398114
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-14 10:44:13 +0800

    Stream消息驱动之生产者

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/org/example/springcloud/controller/SendMessageController.java
@@ -0,0 +1,20 @@
+package org.example.springcloud.controller;
+
+import org.example.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider iMessageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() {
+        return iMessageProvider.send();
+    }
+
+}
