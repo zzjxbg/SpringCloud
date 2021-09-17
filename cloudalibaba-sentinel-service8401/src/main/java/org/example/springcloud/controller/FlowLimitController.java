7fa6fc482fb99de79957262c2e576fba5ed1bc6f
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-17 14:28:44 +0800

    sentinel热点key

--- cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package org.example.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -42,6 +45,19 @@ public class FlowLimitController {
         return "------testE 测试异常数";
     }
 
-
+    /**
+     * @SentinelResource: 处理的是Sentinel控制台配置的违规情况,有blockHandler方法配置的兜底处理
+     * RuntimeException: int age = 10/0,java运行时报出的运行时异常,@SentinelResource不管
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value="p1",required = false) String p1,
+                             @RequestParam(value="p2",required = false) String p2) {
+        return "-------testHotKey";
+    }
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "-----deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认提示:Blocked by Sentinel(flow limiting)
+    }
 
 }
