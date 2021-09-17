523d38225f1708864d01f7a3353cc64ea6ea7644
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-17 13:46:13 +0800

    sentinel降级-异常数

--- cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
@@ -34,4 +34,14 @@ public class FlowLimitController {
         return "------testD";
     }
 
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+
+
 }
