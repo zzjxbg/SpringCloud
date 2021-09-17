7f813a5fad71d25c1529468363967f1139e195b6
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-17 11:10:53 +0800

    sentinel流控-排队等待

--- cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/org/example/springcloud/controller/FlowLimitController.java
@@ -1,11 +1,13 @@
 package org.example.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -17,6 +19,7 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "-----testB";
     }
 }
