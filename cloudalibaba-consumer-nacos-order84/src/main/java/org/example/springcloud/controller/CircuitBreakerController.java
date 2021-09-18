e9c6843464c173739e0e2aa85dbf23b61045ae62
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-18 10:34:33 +0800

    Sentinel服务熔断无配置

--- cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
@@ -1,6 +1,11 @@
 package org.example.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import lombok.extern.slf4j.Slf4j;
+import org.example.springcloud.entities.CommonResult;
+import org.example.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,4 +19,20 @@ public class CircuitBreakerController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback") //没有配置
+    //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
+    //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+//    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
+//            exceptionsToIgnore = {IllegalArgumentException.class})
+    public CommonResult<Payment> fallback(@PathVariable Long id) {
+        CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id, CommonResult.class,id);
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }else if (result.getData() == null) {
+            throw new NullPointerException ("NullPointerException,该ID没有对应记录,空指针异常");
+        }
+        return result;
+    }
 }
--- cloudalibaba-provider-payment9004/src/main/java/org/example/sprnigcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9004/src/main/java/org/example/sprnigcloud/controller/PaymentController.java
@@ -5,9 +5,11 @@ import org.example.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@RestController
 public class PaymentController {
         @Value("${server.port}")
         private String serverPort;
