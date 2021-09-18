6c3495d25e503d595aa35b2a3031c87dc7a62bcf
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-18 11:14:14 +0800

    Sentinel服务熔断同时配置fallback和blockHandler

--- cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
@@ -24,7 +24,8 @@ public class CircuitBreakerController {
     @RequestMapping("/consumer/fallback/{id}")
 //    @SentinelResource(value = "fallback") //没有配置
 //    @SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
-    @SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+//    @SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler") //同时配置fallback和blockHandler
 //    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
 //            exceptionsToIgnore = {IllegalArgumentException.class})
     public CommonResult<Payment> fallback(@PathVariable Long id) {
@@ -38,10 +39,10 @@ public class CircuitBreakerController {
     }
 
     //fallback
-//    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
-//        Payment payment = new Payment(id,"null");
-//        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
-//    }
+    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
+        Payment payment = new Payment(id,"null");
+        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
+    }
 
     //blockHandler
     public CommonResult blockHandler(@PathVariable  Long id, BlockException blockException) {
