25fe14018fc29363ba32c2aca9e63c63b84be4d5
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-18 11:19:57 +0800

    Sentinel服务熔断exceptionsToIgnore

--- cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
@@ -25,9 +25,9 @@ public class CircuitBreakerController {
 //    @SentinelResource(value = "fallback") //没有配置
 //    @SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
 //    @SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
-    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler") //同时配置fallback和blockHandler
-//    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
-//            exceptionsToIgnore = {IllegalArgumentException.class})
+//    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler") //同时配置fallback和blockHandler
+    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
+            exceptionsToIgnore = {IllegalArgumentException.class}) //假如报该异常,不再有fallback兜底方法,没有降级效果
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id, CommonResult.class,id);
         if (id == 4) {
