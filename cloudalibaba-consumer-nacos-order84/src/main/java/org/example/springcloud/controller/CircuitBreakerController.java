1db198db158bae86bb9b27852c40a4deeb107f88
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-18 10:49:39 +0800

    Sentinel服务熔断只配置blockHandler

--- cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/org/example/springcloud/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package org.example.springcloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.example.springcloud.entities.CommonResult;
 import org.example.springcloud.entities.Payment;
@@ -22,8 +23,8 @@ public class CircuitBreakerController {
 
     @RequestMapping("/consumer/fallback/{id}")
 //    @SentinelResource(value = "fallback") //没有配置
-    @SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
-    //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+//    @SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
+    @SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
 //    @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
 //            exceptionsToIgnore = {IllegalArgumentException.class})
     public CommonResult<Payment> fallback(@PathVariable Long id) {
@@ -37,8 +38,15 @@ public class CircuitBreakerController {
     }
 
     //fallback
-    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
+//    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
+//        Payment payment = new Payment(id,"null");
+//        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
+//    }
+
+    //blockHandler
+    public CommonResult blockHandler(@PathVariable  Long id, BlockException blockException) {
         Payment payment = new Payment(id,"null");
-        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
+        return new CommonResult<>(445,"blockHandler-sentinel限流,无此流水: blockException  "+blockException.getMessage(),payment);
     }
+
 }
